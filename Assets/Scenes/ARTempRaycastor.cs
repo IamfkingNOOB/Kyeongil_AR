79a46e9591820d7ca58b2fa5b7a5818bd69219df
danielcho <hellena1717@gmail.com>
2024-06-13 00:05:07 +0900

    [feat] ARRaycast - 참조할 컴포넌트 추가 / ARManager에서 연동
    - XROrigin에 RaycastManager 추가 및 RaycastHit 프리팹 등록
    - ARRaycaster 오브젝트에 XROrigin과, PlaceObjce 프리팹 등록

--- Assets/Scenes/ARTempRaycastor.cs
+++ Assets/Scenes/ARTempRaycastor.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ARTempRaycastor : MonoBehaviour
 {
+    public ARRaycastManager Manager_ARRaycast;
+    public GameObject Prefab_Object;
 
 }
