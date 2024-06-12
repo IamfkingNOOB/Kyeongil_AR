57d5bee0ec4d9bba71eedfbd8f6e32837644f55d
danielcho <hellena1717@gmail.com>
2024-06-13 00:09:58 +0900

    [feat] ARRaycast - 레이쏘기

--- Assets/Scenes/ARTempRaycastor.cs
+++ Assets/Scenes/ARTempRaycastor.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARTempRaycastor : MonoBehaviour
 {
     public ARRaycastManager Manager_ARRaycast;
     public GameObject Prefab_Object;
 
+
+    private void UpdateCenterObject()
+    {
+        Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        Manager_ARRaycast.Raycast(screenCenter, hits, TrackableType.Planes);
+    }
 }
