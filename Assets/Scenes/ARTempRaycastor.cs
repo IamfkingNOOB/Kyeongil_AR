c1906273f503479734a010d6c7c5c9583db2faec
danielcho <hellena1717@gmail.com>
2024-06-13 00:19:57 +0900

    [feat] ARRaycast - 히트 지점에 오브젝트 생성

--- Assets/Scenes/ARTempRaycastor.cs
+++ Assets/Scenes/ARTempRaycastor.cs
@@ -9,12 +9,26 @@ public class ARTempRaycastor : MonoBehaviour
     public ARRaycastManager Manager_ARRaycast;
     public GameObject Prefab_Object;
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdateCenterObject();
+        }
+    }
+
 
     private void UpdateCenterObject()
     {
-        Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        Vector3 screenCenter = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
 
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         Manager_ARRaycast.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        if(hits.Count > 0)
+        {
+            Pose firstHittedPlacePos = hits[0].pose;
+            Instantiate(Prefab_Object, firstHittedPlacePos.position, firstHittedPlacePos.rotation);
+        }
     }
 }
